a1af707b86a3cfc7bb1a465e1aeca963fa302f95
Kalaiyarazan <kalaiyarazan.v@gmail.com>
2019-08-02 13:06:05 +0530

    Add files via upload

---
+++ Leap_or_Not.py
@@ -0,0 +1,13 @@
+year=int(input())
+
+def leap():
+    if((year%400==0) or (year%100!=0)):
+        print("Yes")
+    else:
+        print("No")
+
+if((year%4==0)
+):
+    leap()
+else:
+    print("No")
